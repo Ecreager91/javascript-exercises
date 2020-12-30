5b85b4be27959a5faa555d57bf3bef3c16061167
Eric Creager <eric.creager@yahoo.com>
2020-12-30 10:04:14 -0600

    passes all specs

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
 
-
-
+var repeatedString = "";    
+while (times > 0){
+repeatedString += string
+times--
 }
+if (times < 0){
+    return "ERROR"
+}
+else
+return repeatedString
+}
+
 module.exports = repeatString
