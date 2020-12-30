d2c99a1d70a1e3fc9b90256dcdfa14bf488552db
Eric Creager <eric.creager@yahoo.com>
2020-12-30 09:13:28 -0600

    first attempt with help from freecodecamp

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+var repeatedString = "hey"
 
+while (times > 0){
+    repeatedString += string
+    times--
 }
-
+return repeatedString
+}
 module.exports = repeatString
